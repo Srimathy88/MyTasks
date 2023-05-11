12165009bbe7b3cb9bd56d7e741adf775d5b6045
Srimathy88 <133203877+Srimathy88@users.noreply.github.com>
2023-05-11 13:35:03 +0530

    Add files via upload

---
+++ AccBuildEnq.java
@@ -0,0 +1,35 @@
+package com.itss.buildenq;
+
+import java.util.List;
+
+import com.temenos.t24.api.complex.eb.enquiryhook.EnquiryContext;
+import com.temenos.t24.api.complex.eb.enquiryhook.FilterCriteria;
+import com.temenos.t24.api.hook.system.Enquiry;
+
+/**
+ * TODO: Document me!
+ *
+ * @author 91875
+ *
+ */
+public class AccBuildEnq extends Enquiry {
+
+    @Override
+    public List<FilterCriteria> setFilterCriteria(List<FilterCriteria> filterCriteria, EnquiryContext enquiryContext) {
+        // TODO Auto-generated method stub
+        
+        FilterCriteria fc= new FilterCriteria();
+        fc.setFieldname("CURRENCY");
+        fc.setOperand("EQ");
+        fc.setValue("USD");
+        
+        
+        filterCriteria.add(fc);
+        return filterCriteria;
+        
+       
+    }
+
+    
+    
+}
